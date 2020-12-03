2e2b36b0cd649c043d451af7a31aee793ca22b81
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-03 11:42:36 +0100

    add playerController function to connect player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     private const string AXIS_H = "Horizontal", AXIS_V = "Vertical";
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
         MoveCharacter();
+    }
+
+    void LateUpdate() {
+        SetAnimatorAxisValues();
     }
 
     void MoveCharacter()
@@ -25,5 +35,11 @@ public class PlayerController : MonoBehaviour
             Vector3 translation = new Vector3(0, Input.GetAxisRaw(AXIS_V)* speed * Time.deltaTime, 0);
             this.transform.Translate(translation);
         }
+    }
+
+    void SetAnimatorAxisValues()
+    {
+        _animator.SetFloat(AXIS_H, Input.GetAxisRaw(AXIS_H));
+        _animator.SetFloat(AXIS_V, Input.GetAxisRaw(AXIS_V));
     }
 }
