1162cf78131d831bf0656330c6c7ae21aedb5d9e
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-03 13:37:46 +0100

    add a player a function to camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float cameraSpeed = 5f;
+    private Vector3 targetPosition;
+
+    void Update() 
+    {
+        targetPosition = new Vector3(target.transform.position.x, target.transform.position.y, this.transform.position.z);
+    }
+
+    void LateUpdate()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, Time.deltaTime * cameraSpeed);
+    }
+}
