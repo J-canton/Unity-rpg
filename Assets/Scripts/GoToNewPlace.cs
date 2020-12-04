5bae747ca88eb590f21f37aec7f6f5ebe1745fdc
Juan Luis Cantón Garay <juanluiscantongaray@iMac-de-Juan.local>
2020-12-04 15:31:48 +0100

    new functions

---
+++ Assets/Scripts/GoToNewPlace.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNewPlace : MonoBehaviour
+{
+
+    public string newPlaceName;
+    public string uuid;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Teleport();
+        }
+    }
+
+    private void Teleport()
+    {
+        FindObjectOfType<PlayerController>().nextUuid = uuid;
+        SceneManager.LoadScene(newPlaceName);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public static bool playerCreated;
+
     public float speed = 5f;
     public bool walking = false;
     public Vector2 lastMovement = Vector2.zero;
@@ -14,10 +17,22 @@ public class PlayerController : MonoBehaviour
         LAST_HORIZONTAL = "LastHorizontal",
         LAST_VERTICAL="LastVertical";
     private Animator _animator;
+    private Rigidbody2D _rigidbody;
+    public string nextUuid;
 
     void Start()
     {
         _animator = GetComponent<Animator>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        if(!PlayerController.playerCreated)
+        {
+            DontDestroyOnLoad(this.transform.gameObject);
+            playerCreated  = true;
+        }else
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Update()
@@ -29,22 +44,26 @@ public class PlayerController : MonoBehaviour
         SetCharacterIsWalking(walking);
         SetAnimatorAxisValues();
         SetLastMovementAnimatorAxisValues(lastMovement);
+        StopPlayerMovement();
+
     }
 
     void MoveCharacter()
     {
         if(Mathf.Abs(Input.GetAxisRaw(AXIS_H))>0.02f)
         {
-            Vector3 translation = new Vector3(Input.GetAxisRaw(AXIS_H)* speed * Time.deltaTime, 0,0);
-            this.transform.Translate(translation);
+            //Vector3 translation = new Vector3(Input.GetAxisRaw(AXIS_H)* speed * Time.deltaTime, 0,0);
+            //this.transform.Translate(translation);
+            _rigidbody.velocity = new Vector2(Input.GetAxisRaw(AXIS_H)* speed, _rigidbody.velocity.y);
             walking = true;
             lastMovement = new Vector2(Input.GetAxisRaw(AXIS_H),0);
         }
 
         if(Mathf.Abs(Input.GetAxisRaw(AXIS_V))>0.02f)
         {
-            Vector3 translation = new Vector3(0, Input.GetAxisRaw(AXIS_V)* speed * Time.deltaTime, 0);
-            this.transform.Translate(translation);
+            //Vector3 translation = new Vector3(0, Input.GetAxisRaw(AXIS_V)* speed * Time.deltaTime, 0);
+            //this.transform.Translate(translation);
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, Input.GetAxisRaw(AXIS_V)*speed);
             walking = true;
             lastMovement = new Vector2(0,Input.GetAxisRaw(AXIS_V));
         }
@@ -69,5 +88,13 @@ public class PlayerController : MonoBehaviour
     void  SetCharacterIsWalking(bool newState)
     {
         _animator.SetBool(WALKING, newState);
+    }
+
+    void StopPlayerMovement()
+    {
+        if(!walking)
+        {
+            _rigidbody.velocity = Vector2.zero;
+        }
     }
 }
